0bc83d106059326907072fe01c27e01dfbf921a5
nsmle <fikiproductionofficial@gmail.com>
2022-04-06 04:29:15 +0700

    test pusher2

--- public/js/app.js
+++ public/js/app.js
@@ -5500,7 +5500,6 @@ alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start(); // Reference from publ
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var laravel_echo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! laravel-echo */ "./node_modules/laravel-echo/dist/echo.js");
-/* provided dependency */ var process = __webpack_require__(/*! process/browser.js */ "./node_modules/process/browser.js");
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -5554,7 +5553,7 @@ window.Echo = new Echo({
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "02708a3a1965bba8defd",
-  wsHost: process.env.MIX_PUSHER_WS_HOST,
+  wsHost: window.location.hostname,
   wsPort: 6001,
   forceTLS: false,
   disableStats: true,
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -57,7 +57,7 @@ window.Echo = new Echo({
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_PUSHER_WS_HOST,
+    wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
     disableStats: true,
