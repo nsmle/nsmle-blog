74dd4a4889c3b3a1a74626b5b50c734936ae6a7d
nsmle <fikiproductionofficial@gmail.com>
2022-04-06 04:55:26 +0700

    test pusher3

--- public/js/app.js
+++ public/js/app.js
@@ -5553,6 +5553,7 @@ window.Echo = new Echo({
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "02708a3a1965bba8defd",
+  cluster: "ap1",
   wsHost: window.location.hostname,
   wsPort: 6001,
   forceTLS: false,
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -57,6 +57,7 @@ window.Echo = new Echo({
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
